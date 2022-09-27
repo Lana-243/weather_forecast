9cb666a41210552167c5b94f19e4ab8385fc0e36
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-09-27 20:58:43 +0300

    minor rubocop errors

--- lib/forecast.rb
+++ lib/forecast.rb
@@ -1,8 +1,8 @@
-require "date"
+require 'date'
 
 class Forecast
-  TIME_OF_DAY = %w(night morning day evening).freeze
-  CLOUDINESS = ["clear", "partly cloudy", "cloudy", "mainlycloudy"].freeze
+  TIME_OF_DAY = %w[night morning day evening].freeze
+  CLOUDINESS = %w[clear partly cloudy cloudy mainlycloudy].freeze
 
   def initialize(params)
     @date = params[:date]
@@ -12,36 +12,35 @@ class Forecast
     @cloudiness = params[:cloudiness]
     @max_wind = params[:max_wind]
   end
-  
+
   def self.from_xml(node)
-    day = node.attributes["day"]
-    month = node.attributes["month"]
-    year = node.attributes["year"]
+    day = node.attributes['day']
+    month = node.attributes['month']
+    year = node.attributes['year']
 
     new(
       date: Date.parse("#{day}.#{month}.#{year}"),
-      time_of_day: TIME_OF_DAY[node.attributes["tod"].to_i],
-      temperature_max: node.elements["TEMPERATURE"].attributes["max"].to_i,
-      temperature_min: node.elements["TEMPERATURE"].attributes["min"].to_i,
-      cloudiness: node.elements["PHENOMENA"].attributes["cloudiness"].to_i,
-      max_wind: node.elements["WIND"].attributes["max"].to_i
+      time_of_day: TIME_OF_DAY[node.attributes['tod'].to_i],
+      temperature_max: node.elements['TEMPERATURE'].attributes['max'].to_i,
+      temperature_min: node.elements['TEMPERATURE'].attributes['min'].to_i,
+      cloudiness: node.elements['PHENOMENA'].attributes['cloudiness'].to_i,
+      max_wind: node.elements['WIND'].attributes['max'].to_i
     )
   end
 
   def to_s
-    result = today? ? "Today" : @date.strftime("%d.%m.%Y")
+    result = today? ? 'Today' : @date.strftime('%d.%m.%Y')
 
     result << ", #{@time_of_day}\n" \
-      "#{temperature_range_string}, wind #{@max_wind} м/с, " \
+      "#{temperature_range_string}, wind #{@max_wind} m/s, " \
       "#{CLOUDINESS[@cloudiness]}"
-
   end
 
   def temperature_range_string
-    result = ""
-    result << "+" if @temperature_min > 0
+    result = ''
+    result << '+' if @temperature_min.positive?
     result << "#{@temperature_min}.."
-    result << "+" if @temperature_max > 0
+    result << '+' if @temperature_max.positive?
     result << @temperature_max.to_s
     result
   end
